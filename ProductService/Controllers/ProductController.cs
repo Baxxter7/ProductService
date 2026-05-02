19e0d84b2eabf76921dbe1148b188f62cc9713bc
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-05-01 22:55:55 -0600

    feat: add productController

---
+++ ProductService/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductService.Data;
+using ProductService.Models;
+namespace ProductService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly ProductContext _context;
+
+    public ProductController(ProductContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Product>> PostProduct(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetProducts), new { id = product.Id }, product);
+    }
+
+}
