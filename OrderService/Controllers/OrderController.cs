a4f888d28cf7f5527de1b9ecfb242f43b97cae40
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2026-05-04 23:36:19 -0600

    feat: add OrderService project

---
+++ OrderService/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrderService.Data;
+using OrderService.Models;
+
+namespace OrderService.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController : ControllerBase
+{
+    private readonly OrderContext _orderContext;
+    public OrderController(OrderContext orderContext) {
+        _orderContext = orderContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Order>>> GetOrders() 
+        =>  await _orderContext.Orders.ToListAsync();
+
+    [HttpPost]
+    public async Task<ActionResult<Order>> PostOrder(Order order)
+    {
+        _orderContext.Orders.Add(order);
+        await _orderContext.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetOrders), new { id = order.Id }, order);
+    }
+}
---
+++ OrderService/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using OrderService.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<OrderContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<OrderContext>();
+    dbContext.Database.Migrate();
+}
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
